302174bd6782a0cc39840c17c68d8753ed415eee
zhou <killian.zhou@sepefrei.fr>
2020-09-15 16:12:15 +0200

    stack error timeout

--- lib/stack.h
+++ lib/stack.h
@@ -7,15 +7,15 @@
 
 #include "SLL.h"
 
-typedef struct
-{
-    Node* top;
-}Stack;
-
-int stackGet(Stack* stack);
-void stackPushNode(Stack* stack, Node* node);
-void stackPushValue(Stack* stack, int value);
-void stackRemove(Stack* stack);
-int stackIsEmpty(Stack* stack);
+//typedef struct
+//{
+//    Node* top;
+//}Stack;
+//
+//int stackGet(Stack* stack);
+//void stackPushNode(Stack* stack, Node* node);
+//void stackPushValue(Stack* stack, int value);
+//void stackRemove(Stack* stack);
+//int stackIsEmpty(Stack* stack);
 
 #endif //TD_EN_C_STACK_H
