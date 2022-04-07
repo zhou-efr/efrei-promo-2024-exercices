f52f22e19e40fdfbc2d5fbefe1eebef3328f619d
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-04-07 13:55:38 +0800

    Computer Architecture

--- Semestre 6 - Java - Web - OS/CAR/qus3.asm
+++ Semestre 6 - Java - Web - OS/CAR/qus3.asm
@@ -7,18 +7,19 @@
 Main proc
     mov ax,@data
     mov ds,ax
+    
 
     ; Display an ASCII character on screen using DOS interrupt 21h.
-    mov dl, 235 ; weird ascii
-    mov ah, 02h
-    int 21h
+    mov dl, 112 ; p character
+    mov ah, 02h ; character display
+    int 21h ; DOS interrupt 21h
+    
+    ; Display an ASCII character on screen using DOS interrupt 10h.
+    mov ah,0Eh  ; bios function 0eh to teletype character
+    mov al,235 ; al will contain the value of the memory location pointed by si
+    int 10h     ; bios interrupt 10h
 
-    ; Display an ASCII character on screen using BIOS interrupt 10h.
-    mov dl, 10 ; ascii new line
-    mov ah, 02h
-    int 10h
-
-    mov dl, 13 ; ascii return carrier
+    mov dl, 10
     mov ah, 02h
     int 21h
 
@@ -27,13 +28,13 @@ Main proc
     mov dx,offset panda
     int 21h
 
-    mov dl, 10 ; ascii new line
+    mov dl, 10
     mov ah, 02h
     int 21h
 
     ; Read String from keyboard using DOS interrupt 21h
     mov ah,0Ah ; Interrupt 21h/Function 0Ah (DOS)
-    int 21h ; from keyboard, store in AL
+    int 21h
     
     mov ah,4ch
     int 21h
